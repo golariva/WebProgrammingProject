1732d9d7370e0700096947170b81c8868faadf58
madvicspar <viktoria.akkuzina@yandex.ru>
2025-06-20 09:13:18 +0500

    fix: подключение к бд

--- back/restaurants_booking_app/app/database.py
+++ back/restaurants_booking_app/app/database.py
@@ -5,6 +5,14 @@ from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
+db_host = "localhost"Add commentMore actions
+db_port = 5432
+db_user = "postgres"
+db_pass = "postgres"
+db_name = "restraunts_booking"
+
+database_url = f"postgresql+asyncpg://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+
 database_url = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(database_url)
